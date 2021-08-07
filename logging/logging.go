75cfa6ab28155a7b36a458607b8a49b801998828
Colin Wilcox <colinwilcox1967@gmail.com>
2021-08-07 16:34:30 +0100

    add error capture and return

--- logging/logging.go
+++ logging/logging.go
@@ -14,19 +14,20 @@ type LogFileSession struct {
 
 var logsession LogFileSession
 
-func InitLog (logfilename string) {
+func InitLog (logfilename string) error {
 
 	var err error
 
 	logsession.LogFileHandle, err = os.OpenFile(logfilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
     if err != nil {
-        log.Fatal(err)
+       return err
     }
 
 	log.SetOutput(logsession.LogFileHandle)
 	log.SetFlags (log.Ldate|log.Ltime)
 
 	logsession.LogInitialised = true
+	return nil
 }
 
 func DoWriteString (str string, writeToConsole, writeToLog bool) {
