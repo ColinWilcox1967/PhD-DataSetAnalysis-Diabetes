9089470af5d91438851b51695d51fa6a73b0f231
Colin Wilcox <colinwilcox1967@gmail.com>
2021-10-02 17:20:43 +0100

    Add file loader and parser code

--- classifier/classifier.go
+++ classifier/classifier.go
@@ -1,10 +1,51 @@
 package classifier
 
+import (
+	"os"
+	"bufio"	
+	"strings"
+) 
 
 var Metrics ClassifierMetrics
 
-func LoadClassifierFromFile () bool {
-return false
+func LoadClassifierFromFile (filepath string) ([]string, error) {
+	file, err := os.Open(filepath)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
+}
+
+func SetClassifierMetrics (metrics []string) bool {
+
+	for _, line := range (metrics) {
+		if line[0] != '#' { // not a comment
+		
+			str := strings.ToUpper (line)
+			if strings.Contains (str, "N=") {
+				//Number of neighbours
+				return true
+			} 
+			if strings.Contains (str, "TP=") {
+				// True Positive Threshold
+				return true
+			}	 
+
+			if strings.Contains (str, "TN=") {
+				// True Negative Threshold
+				return true
+			}
+		}
+	}
+
+	return false
 }
 
 func ChangeOutcomeValue (neighbours []int, value int) bool {
