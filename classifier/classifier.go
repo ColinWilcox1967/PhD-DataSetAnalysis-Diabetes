e8de9659444443347dd7d3fb3a5457fd864f513e
Colin Wilcox <colinwilcox1967@gmail.com>
2022-02-13 18:19:14 +0000

    change classifier

--- classifier/classifier.go
+++ classifier/classifier.go
@@ -1,29 +1,27 @@
 package classifier
 
 import (
+	"bufio"
 	"os"
-	"bufio"	
-	"strings"
 	"strconv"
-) 
+	"strings"
+)
 
 const (
-	default_number_of_neighbours = 7
-	default_tpthreshold = 5
-	default_tnthreshold=2
+	default_number_of_neighbours = 5
+	default_tpthreshold          = 3
+	default_tnthreshold          = 2
 )
 
 var ThresholdClassifier ClassifierMetrics
 
-
-
-func setDefaultClassifierMetrics () {
+func setDefaultClassifierMetrics() {
 	ThresholdClassifier.NumberOfNeighbours = default_number_of_neighbours
 	ThresholdClassifier.TPThreshold = default_tpthreshold
 	ThresholdClassifier.TNThreshold = default_tnthreshold
 }
 
-func countNeighbours (neighbours []int, value int) int {
+func countNeighbours(neighbours []int, value int) int {
 
 	if len(neighbours) < ThresholdClassifier.NumberOfNeighbours {
 		return 0
@@ -39,65 +37,60 @@ func countNeighbours (neighbours []int, value int) int {
 	return count
 }
 
-func LoadClassifierFromFile (filepath string) ([]string, error) {
+func LoadClassifierFromFile(filepath string) ([]string, error) {
 	file, err := os.Open(filepath)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-    return lines, scanner.Err()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
-func SetClassifierMetrics (metrics []string) {
+func SetClassifierMetrics(metrics []string) {
 
-	setDefaultClassifierMetrics () // set to defaults so we have something consistent
-	for _, line := range (metrics) {
+	setDefaultClassifierMetrics() // set to defaults so we have something consistent
+	for _, line := range metrics {
 		if line[0] != '#' { // not a comment
 			//Number of neighbours
-			str := strings.ToUpper (line)
-			if strings.Contains (str, "N=") {
+			str := strings.ToUpper(line)
+			if strings.Contains(str, "N=") {
 				parts := strings.Split(str, "=")
-				
+
 				n := parts[1]
-				v, err := strconv.Atoi (n)
+				v, err := strconv.Atoi(n)
 				if err != nil {
 					ThresholdClassifier.NumberOfNeighbours = v
 				}
 
-				
-			
-			} 
-			if strings.Contains (str, "TP=") {
+			}
+			if strings.Contains(str, "TP=") {
 				// True Positive Threshold
 				parts := strings.Split(str, "=")
 				tp := parts[1]
-				v,err := strconv.Atoi (tp)
+				v, err := strconv.Atoi(tp)
 				if err != nil {
 					ThresholdClassifier.TPThreshold = v
 				}
-				
-			}	 
 
-			if strings.Contains (str, "TN=") {
+			}
+
+			if strings.Contains(str, "TN=") {
 				// True Negative Threshold
 				parts := strings.Split(str, "=")
 				tn := parts[1]
-				v,err := strconv.Atoi(tn)
+				v, err := strconv.Atoi(tn)
 				if err != nil {
 					ThresholdClassifier.TNThreshold = v
 				}
-				
+
 			}
 		}
 	}
 
-	
 }
-
-
